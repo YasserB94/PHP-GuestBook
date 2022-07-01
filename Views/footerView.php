e11b4def1f82f8905d43bd3988f73ee87252ebbe
Yasser Bal <yasserbal@icloud.com>
2022-07-01 11:32:42 +0200

    Added closing for containers uswed to display posts

--- Views/footerView.php
+++ Views/footerView.php
@@ -1,6 +1,17 @@
 <?php
         declare(strict_types=1);
 ?>
-
+<!--CLOSE CONTAINERS FOR POSTS OVERVIEW-->
+</div>
+</div>
+</section>
+<footer class="text-gray-600 body-font">
+  <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 
+        Yasser Bal — 
+      <a href="https://github.com/YasserB94/PHP-GuestBook" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
+    </p>
+  </div>
+</footer>
 </body>
 </html>
