bea9c896a83f20edaf4322d308707e9f9b3dd34f
Yasser Bal <yasserbal@icloud.com>
2022-07-01 14:49:14 +0200

    Removed Title/Navbar

--- Views/headerView.php
+++ Views/headerView.php
@@ -12,7 +12,7 @@
 </head>
 <body>
 <header class="text-gray-600 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+  <!-- <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-red-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
@@ -30,5 +30,5 @@
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
     </button>
-  </div>
+  </div> -->
 </header>
