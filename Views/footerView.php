b626ae5d1a0df96bdfbd927332703c444f34f19c
Yasser Bal <yasserbal@icloud.com>
2022-07-04 12:04:05 +0200

    Current year will now always properly display in copyright disclaimer

--- Views/footerView.php
+++ Views/footerView.php
@@ -7,7 +7,7 @@
 </section>
 <footer class="text-gray-600 body-font">
   <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 
+    <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© <?=date('Y')?> 
         Yasser Bal — 
       <a href="https://github.com/YasserB94/PHP-GuestBook" class="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">GitHub Repository</a>
     </p>
